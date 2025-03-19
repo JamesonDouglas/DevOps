912098756ad91a473c7dcb853db13afeb035be66
JamesonDouglas <jamesondouglas@hotmail.com>
2025-03-18 22:00:44 -0300

    modificação
    primeira aula de edição usando branch

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return{"message": "bom dia"}
+
+@app.get("/modificacao")
+async def funcaoteste():
+    return {"teste": True, "num_aleatorio": random.randint(a:0, b:1000)}

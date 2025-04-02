de49f426914aefb54f4a1f39b50ee634158b0a90
JamesonDouglas <jamesondouglas@hotmail.com>
2025-04-02 20:33:58 -0300

    Criação do Continuos integration

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI # type: ignore
+import random
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return{"message": "bom dia"}
+
+@app.get("/teste")
+async def teste():
+    return {"teste": True, "num_aleatorio": random.randint(1, 100)}
